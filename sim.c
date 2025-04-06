9ab49242cebd692f40343a9b5c5327e8d36c73d6
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-04-06 07:50:11 +0200

    Struct for general purpose registers

---
+++ sim.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+struct general_purpose_registers_32 {
+	uint32_t eax;
+	uint32_t ebx;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t esi;
+	uint32_t edi;
+	uint32_t esp;
+	uint32_t ebp;
+};
+
+int main(){
+	
+}
