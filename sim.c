51a4ae7e216a469f905cc332ab4349f9a0185e78
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-04-18 19:36:30 +0200

    Basic SDL Window

--- sim.c
+++ sim.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 struct general_purpose_registers_32 {
 	uint32_t eax;
@@ -111,6 +112,58 @@ uint64_t rflags;
 //BNDCFGU Register
 //BNDSTATUS Register
 
-int main(){
-	
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+float x, y;
+const char *title = "x86_64 Emulator";
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    /* Create the window */
+    if (!SDL_CreateWindowAndRenderer("x86_64 Emulator", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
+        SDL_Log("Couldn't create window and renderer: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_ESCAPE ||
+        event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+	int w = 0, h = 0;
+    const float scale = 2.0f; 
+
+    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(title)) * 0.5;
+    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) * 0.1;
+    
+    /* Center the title and scale it up */
+    SDL_GetRenderOutputSize(renderer, &w, &h);
+    SDL_SetRenderScale(renderer, scale, scale);
+    
+    /* Draw the title */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDebugText(renderer, x, y, title);
+ 
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
 }
